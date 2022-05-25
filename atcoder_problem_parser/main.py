c3014004951f90ffe1d28feddd054d6d666b7a73
fjnkt98 <fjnkt98@gmail.com>
2022-05-25 22:04:33 +0900

    Add comments

--- atcoder_problem_parser/main.py
+++ atcoder_problem_parser/main.py
@@ -5,11 +5,23 @@ from typing import List
 import sys
 
 
-def parse(text: str):
+def parse(text: str) -> List[str]:
+    """Parse the html and extract destination paragraphs
+
+    Args:
+        - text (str): HTML document for parse.
+
+    Returns:
+        - List[str]: The result of parse.
+    """
+    # Parse the html document by using BeautifulSoup4
     soup = bs4.BeautifulSoup(text, "html.parser")
 
+    # List for containing the parse result
+    # At first, try to extract problem statements
     result: List[str] = ["### 問題文", ""]
     for p in soup.select("h3:-soup-contains('問題文') ~ p"):
+        # Buffer list
         strings: List[str] = []
         for t in p:
             if isinstance(t, bs4.element.NavigableString):
@@ -20,15 +32,25 @@ def parse(text: str):
                 elif t.name == "code":
                     strings.append("`" + str(t.get_text(strip=True)) + "`")
                 elif t.name == "br":
+                    # WHen <br> tag is there, break line.
+                    # Store the result in citation format
                     result.append("> " + "".join(strings))
                     strings = []
+
         if strings:
+            # If the buffer is not empty, extract all contents
+            # Store the result in citation format
             result.append("> " + "".join(strings))
         result.append("> ")
+
+    # Eliminate last line
     result[-1] = ""
 
+    # Next, try to extract problem constraints statements
     result.extend(["### 制約", ""])
 
+    # Extract logic is same as above
+    # Assuming that <br> tag doesn't exist in constraints statements
     constraint = soup.select("h3:-soup-contains('制約') ~ ul > li")
     for p in constraint:
         strings = []
@@ -75,20 +97,28 @@ def app(contest: str = "", problem: str = "", url: str = "") -> List[str]:
 
 
 def validate(context: click.Context, parameter: click.Parameter, value: str):
+    """Validate function for command argument"""
+
+    # When the value (contest name) is empty, let it pass for direct url specification
     if value == "":
         return value
 
+    # Copy the value
     contest: str = value
 
     if len(contest) != 6:
+        # Length of the contest name should be 6.
         raise click.BadParameter("Invalid contest name.")
     if contest[:3].lower() not in ["abc", "arc", "agc"]:
+        # The first three letters must be fixed.
         raise click.BadParameter(
             "Invalid contest name. This parameter must be 'abc', 'arc', or 'agc'."
         )
     if not (0 <= int(contest[3:]) < 1000):
+        # The last 3 letters must be within the range.
         raise click.BadParameter("Invalid contest number.")
 
+    # Return lowercased string to simplify code.
     return contest.lower()
 
 
@@ -108,9 +138,19 @@ def validate(context: click.Context, parameter: click.Parameter, value: str):
 )
 @click.option("--url", default="", help="Problem URL.")
 def main(contest: str = "", problem: str = "", url: str = "") -> None:
+    """main function
+
+    Args:
+        - contest (str): Contest name. letter case is ignored. e.g.) abc123, arc200, agc021
+        - problem (str): Problem index. Lowercase letters from a to h are only allowed.
+        - url (str): URL of the problem. When this argument is not empty, it takes precedence
+        and the other positional arguments are ignored.
+    """
     try:
+        # Get the parse result.
         result = app(contest, problem, url)
 
+        # Output the result to stdout.
         for r in result:
             print(r)
     except requests.HTTPError:
